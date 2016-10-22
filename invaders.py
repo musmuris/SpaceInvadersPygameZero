87f23819080eccf8c45acbe5e0fe4bab426cfb41
Nigel Morse <nigelm@zepler.org>
2016-10-22 18:55:37 +0100

    Refactor update and add an alien moving

--- invaders.py
+++ invaders.py
@@ -9,8 +9,14 @@ missile.fired = False
 
 alien_a = Actor('alien_a_1')
 alien_a.pos = 250, 200
+alien_a.image1 = 'alien_a_1'
+alien_a.image2 = 'alien_a_2'
+alien_a.frame = 1
+alien_a.direction = 10
 
-def update():
+speed = 30
+
+def updatePlayer():
 	if keyboard[keys.LEFT] and ship.left > 0:
 		ship.left -= 2
 	if keyboard[keys.RIGHT] and ship.right < WIDTH:
@@ -19,12 +25,36 @@ def update():
 		missile.fired = True
 		missile.pos = ship.midtop
 
+def updateMissile():
 	if missile.fired:
 		missile.top -= 5
 		if missile.bottom < 0:
 			missile.fired = False
 
+def updateAlien(alien):
+	global speed
 
+	alien.frame = alien.frame + 1
+	if alien.frame == speed:
+		alien.frame = 1
+		alien.left += alien.direction
+		if alien.left > 420:
+			alien.direction = -10
+		elif alien.left < 20:
+			alien.direction = 10
+
+		if alien.image == alien.image1:
+			alien.image = alien.image2
+		else:
+			alien.image = alien.image1
+
+
+def update():
+	updatePlayer()
+	updateMissile()
+	updateAlien(alien_a)	
+	
+	
 def draw():
 	screen.clear()
 	ship.draw()
