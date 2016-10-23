6f7ba22188b6b7b725b124d9308adbb9b3554d55
Nigel Morse <nigelm@zepler.org>
2016-10-23 18:57:11 +0100

    more aliens

--- invaders.py
+++ invaders.py
@@ -7,15 +7,23 @@ ship.midbottom = 250,490
 missile = Actor('missile')
 missile.fired = False
 
-alien_a = Actor('alien_a_1')
-alien_a.pos = 250, 200
-alien_a.image1 = 'alien_a_1'
-alien_a.image2 = 'alien_a_2'
-alien_a.frame = 1
-alien_a.direction = 10
-alien_a.dead = 0
+aliens = []
+for row in range(5):
+	for col in range(8):
+		alien = Actor('alien_a_1')
+		alien.pos = 55 * col + 50, 40 * row + 100
+		alien.image1 = 'alien_a_1'
+		alien.image2 = 'alien_a_2'
+		alien.frame = 1		
+		alien.dead = 0
+		aliens.append(alien)
 
-speed = 60
+class Game:
+	speed = 41
+	direction = 10
+	flipDirection = False
+
+game = Game()
 
 def updatePlayer():
 	if keyboard[keys.LEFT] and ship.left > 0:
@@ -33,21 +41,17 @@ def updateMissile():
 			missile.fired = False
 
 def updateAlien(alien):
-	global speed
+	if alien.dead > 0:
+		return
 
 	alien.frame = alien.frame + 1
-	if alien.frame == speed:
-		alien.frame = 1
-		updateFrame = True
-	else:
-		updateFrame = False
 
-	if updateFrame:
-		alien.left += alien.direction
-		if alien.left > 420:
-			alien.direction = -10
-		elif alien.left < 20:
-			alien.direction = 10
+	if alien.frame >= game.speed:
+		alien.frame = 1
+		alien.left += game.direction
+
+		if alien.left > 420 or alien.left < 20:
+			game.flipDirection = True
 
 		if alien.image == 'alien_dead':
 			alien.dead += 1
@@ -56,21 +60,27 @@ def updateAlien(alien):
 		else:
 			alien.image = alien.image1
 
-	if alien.colliderect(missile):
+	if alien.colliderect(missile) and missile.fired:
 		alien.image = 'alien_dead'
+		missile.fired = False
+		game.speed -= 1
 
 
 def update():
 	updatePlayer()
 	updateMissile()
-	updateAlien(alien_a)	
-	
+	for alien in aliens:
+		updateAlien(alien)	
+	if game.flipDirection:
+		game.direction = -game.direction
+		game.flipDirection = False
 	
 def draw():
 	screen.clear()
 	ship.draw()
-	if alien_a.dead < 2:
-		alien_a.draw()
+	for alien in aliens:
+		if alien.dead < 1:
+			alien.draw()
 	if missile.fired:
 		missile.draw()
 
